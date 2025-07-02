5b69988b90dd3905d9f55fe14b94c31853f89a80
chrishalcrow <chrishalcrow@gmail.com>
2025-07-02 16:40:46 +0100

    name_script

--- src/eddie_helper/make_scripts.py
+++ src/eddie_helper/make_scripts.py
@@ -21,8 +21,10 @@ def run_stage_script(stageout_dict, script_file_path=None, hold_jid=None, job_na
 
     if hold_jid is not None:
         hold_script = f" -hold_jid {hold_jid}"
-    if job_name is not None:
-        job_name = f" -N {job_name}"
+    if job_name is None:
+        job_name = "stage"
+
+    name_script = f" -N {job_name}"
 
     """
     makes a stage out script from a stageout_dict of the form
@@ -33,7 +35,7 @@ def run_stage_script(stageout_dict, script_file_path=None, hold_jid=None, job_na
     script_text=f"""#!/bin/sh
 #$ -cwd
 #$ -q staging
-#$ -l h_rt=00:29:59{hold_script}{job_name}"""
+#$ -l h_rt=00:29:59{hold_script}{name_script}"""
 
     if script_file_path is None:
         script_file_path = f"{job_name}" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".sh"
