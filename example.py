3231ecd2f4113734819bc0c0b1fad96feba220f7
chrishalcrow <chrishalcrow@gmail.com>
2025-07-02 16:38:34 +0100

    job name starts with M

--- example.py
+++ example.py
@@ -10,5 +10,7 @@ for mouse, days in mouse_days.items():
             f'/exports/eddie/scratch/chalcrow/harry/data/M{mouse}/D{day}/results/': '/exports/cmvm/datastore/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/Chris/Cohort12/derivatives/M{mouse}/D{day}/'
         }
 
-        run_python_script(f"sort.py {mouse} {day} {sorter_name}", username="chalcrow", cores=8, job_name=f"{mouse}D{day}_sort")
-        run_stage_script(stageout_dict, hold_jid=f"{mouse}D{day}_sort")
+        job_name = f"M{mouse}D{day}_sort"
+
+        run_python_script(f"sort.py {mouse} {day} {sorter_name}", username="chalcrow", cores=8, job_name=job_name)
+        run_stage_script(stageout_dict, hold_jid=job_name)
